6a93a23605fed2bab185efcda471d8b22a55e759
黃家楹 <dallas@huangjiayingde-macbook-air.local>
2018-08-28 21:40:11 +0800

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by 黃家楹 on 2018/8/28.
+//  Copyright © 2018年 dallas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
